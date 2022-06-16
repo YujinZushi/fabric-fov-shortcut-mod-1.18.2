8e3cfcc0328e5107164dea3923dedea61dd100e2
ZUSHI <shiganov.e@rambler.ru>
2022-06-16 23:32:55 +0300

    Add client entrypoint

---
+++ src/main/java/net/yujinzushi/fovshortcutmod/FovShortcutModClient.java
@@ -0,0 +1,11 @@
+package net.yujinzushi.fovshortcutmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FovShortcutModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
